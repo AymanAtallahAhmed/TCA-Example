37e0c2812652e219198b769fe02167ef48d6f657
Ayman <ayman.atallah@robustastudio.com>
2024-05-17 17:31:37 +0300

    Initial Commit

---
+++ TCATask/TCATaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATaskApp.swift
+//  TCATask
+//
+//  Created by Ayman Atallah on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCATaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
